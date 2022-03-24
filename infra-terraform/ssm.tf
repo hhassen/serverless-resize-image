b285545cb46db1ba00b80380d2bd3061aeace872
hhassen <hhtun21@yahoo.fr>
2022-03-24 10:41:05 +0100

    add ssm parameter

---
+++ infra-terraform/ssm.tf
@@ -0,0 +1,6 @@
+resource "aws_ssm_parameter" "endpoint" {
+  name        = "/${var.project}/s3/name"
+  description = "${var.bucket_name} BUCKET NAME"
+  type        = "String"
+  value       = "${module.s3_bucket.s3_bucket_id}"
+}
