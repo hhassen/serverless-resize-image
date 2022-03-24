1b87e827704bc7227be34ceb6a8ed4b775f95100
hhassen <hhtun21@yahoo.fr>
2022-03-24 10:46:19 +0100

    terraform fmt

--- infra-terraform/ssm.tf
+++ infra-terraform/ssm.tf
@@ -2,5 +2,5 @@ resource "aws_ssm_parameter" "endpoint" {
   name        = "/${var.project}/s3/name"
   description = "${var.bucket_name} BUCKET NAME"
   type        = "String"
-  value       = "${module.s3_bucket.s3_bucket_id}"
+  value       = module.s3_bucket.s3_bucket_id
 }
